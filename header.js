c44c0dcfcac612660d9d5ebfd5ec4410586d2e07
genuineabrar <genuineabrar@gmail.com>
2026-01-13 13:25:00 +0530

    Update header.js

--- header.js
+++ header.js
@@ -1,6 +1,8 @@
-const toggle = document.getElementById("navToggle")
-const menu = document.getElementById(gi28Menu);
+
+const toggle = document.getElementById("navToggle");
+const menu = document.getElementById("gi28Menu");
+
 toggle.addEventListener("click", () => {
-  toggle.classListt.toggle("active");
+  toggle.classList.toggle("active");
   menu.classList.toggle("open");
 });
